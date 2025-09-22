d3d9ed8a89a909f46456b3d8b757ec73cba04475
YoussefCh9 <maranguren027@ikasle.ehu.eus>
2025-09-22 15:32:08 +0200

    lab1

--- IntroExercises/Exercises.cs
+++ IntroExercises/Exercises.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +14,29 @@ namespace IntroExercises
         //Find should return the position in array where value appears for the first time. -1 if the value isn't found or the array is empty/null
         public static int Find(int[] array, int value)
         {
-            return 0;
+            for (int i = 0; i <array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         //TODO #2
         //Count should return the number of times value appears in array. 0 if the array is empty/null
         public static int Count(int[] array, int value)
         {
-            return 0;
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         //TODO #3
@@ -27,7 +44,31 @@ namespace IntroExercises
         //-1 if endIndex is less than startIndex or any of them is outside the array
         public static int Find(int[] array, int value, int startIndex, int endIndex)
         {
-            return 0;
+            //int i = 0;
+            if (array == null)
+            {
+                return -1;
+            }
+
+            //else if (i < endIndex && i > startIndex)
+            //{
+            /* while (array[i] != value && i < endIndex && i > startIndex)
+             /    i++;
+             if (array[i] == value)
+             {
+                 return i;
+             }
+             */
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+
         }
 
         //TODO #4
@@ -36,7 +77,16 @@ namespace IntroExercises
 
         public static int Count(int[] array, int value, int startIndex, int endIndex)
         {
-            return 0;
+            int count = 0;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (array[i] == value)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         //TODO #5
@@ -49,7 +99,18 @@ namespace IntroExercises
         //  AreEqual(null, null) => false
         public static bool AreEqual(int[] A, int[] B)
         {
-            return true;
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                    if (Count(A, A[i])==Count(B,B[i]))
+                    {
+                        return true;
+                    }
+            }
+            return false;
         }
     }
 }
